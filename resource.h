aa9b838c2ac5eac01c51e654678f0b7a0fe317d0
muzaffar <muzaffar@junggu-da.com>
2022-10-26 11:36:04 +0900

    3 commit

--- resource.h
+++ resource.h
@@ -1,12 +1,19 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by mdi.rc
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// mdi.rc에서 사용되고 있습니다.
+//
+#define IDB_PNG1                        101
+#define IDB_PNG2                        102
+#define IDB_PNG3                        103
+#define IDB_PNG4                        104
+#define IDB_PNG5                        105
+#define IDB_PNG6                        106
 
-// 다음은 새 개체에 사용할 기본값입니다.
+// Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_RESOURCE_VALUE        107
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
